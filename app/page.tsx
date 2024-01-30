fbc57aaf4980e2717663341d3b41603d8d7dcc71
sagardwivedi <sagar.dw20@gmail.com>
2024-01-31 00:18:15 +0530

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { LoginForm } from '@/components/Form/Login';
-
 export default function Home() {
-  return (
-    <div className='flex justify-center min-h-screen items-center'>
-      <div className='w-[90%] max-w-md'>
-        <LoginForm />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            In Progress
+        </div>
+    );
 }
