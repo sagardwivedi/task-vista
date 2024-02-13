a3ec41ea4d1cef42c1500960622638fe4a98c408
sagardwivedi <sagar.dw20@gmail.com>
2024-02-13 21:55:46 +0530

    redesigning the UI and Show and hide feature

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -3,30 +3,30 @@ import { SidebarIcon } from 'lucide-react';
 import { fetchBoards } from '@/lib/actions';
 import { ActiveLink } from '../ui/ActiveLink';
 import { Each } from '../ui/Each';
+import { ScrollArea } from '../ui/scroll-area';
 import { NewBoardModal } from './NewBoardModal';
 
 export async function Sidebar() {
-  const { data } = await fetchBoards();
+  const { board } = await fetchBoards();
 
   return (
     <div className="h-full w-[18rem] border-r px-2 max-md:hidden">
-      <div className="flex h-20 items-center justify-center">
-        <p className="text-2xl font-semibold">Task Vista</p>
-      </div>
+      <NewBoardModal />
       <div>
-        <h2 className="mb-2 px-5">ALL BOARDS ({data?.length})</h2>
-        <Each
-          of={data!}
-          render={(board) => (
-            <ActiveLink key={board.id} href={`/board/${board.id}`}>
-              <SidebarIcon />
-              <p className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap">
-                {board.name}
-              </p>
-            </ActiveLink>
-          )}
-        />
-        <NewBoardModal />
+        <h2 className="mb-2 px-5">ALL BOARDS ({board?.length})</h2>
+        <ScrollArea className="h-[550px]">
+          <Each
+            of={board!}
+            render={(board) => (
+              <ActiveLink key={board.id} href={`/board/${board.id}`}>
+                <SidebarIcon />
+                <p className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap">
+                  {board.name}
+                </p>
+              </ActiveLink>
+            )}
+          />
+        </ScrollArea>
       </div>
     </div>
   );
