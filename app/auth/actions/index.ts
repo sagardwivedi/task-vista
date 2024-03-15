2659a26f78f0ffb01d4f1f7e2b700bed2106aadc
sagardwivedi <sagar.dw20@gmail.com>
2024-03-15 10:02:36 +0530

    rfc: fix styling

--- app/auth/actions/index.ts
+++ app/auth/actions/index.ts
@@ -5,20 +5,20 @@ import { signupFormType } from '@/components/Form/SignUp';
 import { createSerClient } from '@/lib/utils/supabase/server';
 
 export async function signup(data: signupFormType) {
-    const supabase = createSerClient();
+  const supabase = createSerClient();
 
-    const result = await supabase.auth.signUp({
-        email: data.email,
-        password: data.password,
-    });
+  const result = await supabase.auth.signUp({
+    email: data.email,
+    password: data.password,
+  });
 
-    return JSON.stringify(result);
+  return JSON.stringify(result);
 }
 
 export async function signin(data: loginFormType) {
-    const supabase = createSerClient();
+  const supabase = createSerClient();
 
-    const result = await supabase.auth.signInWithPassword(data);
+  const result = await supabase.auth.signInWithPassword(data);
 
-    return JSON.stringify(result);
+  return JSON.stringify(result);
 }
--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/lib/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-    return await updateSession(request);
+  return await updateSession(request);
 }
 
 export const config = {
-    matcher: ['/board/:path*'],
+  matcher: ['/board/:path*'],
 };
